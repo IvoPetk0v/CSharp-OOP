4b1efcd784fada315a4eb1b6baa22ebad466967e
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-07-09 13:20:36 +0300

    upload homeworks

---
+++ Exercise-Encapsulation/1.Class_Box_Data/Box.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace _1.Class_Box_Data
+{
+    public class Box
+    {
+        private const double BoxPropMinValue = 0;
+
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length,double width,double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get => this.length;
+            private set
+            {
+                if (value <= BoxPropMinValue)
+                {
+                    throw new ArgumentException($"{nameof(this.Length)} cannot be zero or negative.");
+                }
+                this.length = value;
+            }
+        }
+        public double Width
+        {
+            get => this.width;
+            private set
+            {
+                if (value <= BoxPropMinValue)
+                {
+                    throw new ArgumentException($"{nameof(this.Width)} cannot be zero or negative.");
+                }
+                this.width = value;
+            }
+        }
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                if (value <= BoxPropMinValue)
+                {
+                    throw new ArgumentException($"{nameof(this.Height)} cannot be zero or negative.");
+                }
+                this.height = value;
+            }
+        }
+
+       public double SurfaceArea() // 2lw + 2lh + 2wh
+        {
+            double surfaceArea = 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+            return surfaceArea;
+        }
+       public double LateralSurfaceArea() // 2lh + 2wh
+        {
+            double lateralSurfaceArea = 2 * (this.length * this.height + this.width * this.height);
+            return lateralSurfaceArea;
+        }
+       public double Volume() // lwh
+        {
+            double volume = this.length * this.width * this.height;
+            return volume;
+        }
+        public override string ToString()
+        {
+            return $"Surface Area - {this.SurfaceArea():F2}{Environment.NewLine}" +
+                   $"Lateral Surface Area - {this.LateralSurfaceArea():F2}{Environment.NewLine}" +
+                   $"Volume - {this.Volume():F2}{Environment.NewLine}";
+        }
+    }
+}
---
+++ Exercise-Encapsulation/1.Class_Box_Data/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1.Class_Box_Data
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double length = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                Box box = new Box(length, width, height);
+                Console.WriteLine(box);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+
+        }
+    }
+}
