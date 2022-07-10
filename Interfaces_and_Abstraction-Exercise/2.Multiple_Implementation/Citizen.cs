f6f403a6e5fb7279c61ed1ac22afc819facdcfe2
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-07-10 16:37:32 +0300

    uploading solutions

---
+++ Interfaces_and_Abstraction-Exercise/2.Multiple_Implementation/Citizen.cs
@@ -0,0 +1,17 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson, IBirthable, IIdentifiable
+    {
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Birthdate = birthdate;
+            this.Id = id;
+        }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Birthdate { get; private set; }
+        public string Id { get; private set; }
+    }
+}
