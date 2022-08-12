e85b738000101ca3cc698d004694c689887b3f86
IvoPetk0v <ipetk0v@students.softuni.bg>
2022-08-13 01:47:48 +0300

    uploading homework

--- UnitTesting-Exercises/FightingArena.Tests/ArenaTests.cs
+++ UnitTesting-Exercises/FightingArena.Tests/ArenaTests.cs
@@ -1,10 +1,71 @@
-﻿namespace FightingArena.Tests
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FightingArena.Tests
 {
     using NUnit.Framework;
 
     [TestFixture]
     public class ArenaTests
     {
+        private Arena arena;
+
+        [SetUp]
+        public void Start()
+        {
+            this.arena = new Arena();
+        }
+        //Testing Ctor
+        [Test]
+        public void ConstructorShouldInitializeEmptyWarriorList()
+        {
+            Assert.AreEqual(new List<Warrior>(), this.arena.Warriors.ToList());
+            Assert.AreEqual(0,this.arena.Count);
+        }
+
+        [Test]
+        public void EnrollShouldThrowExceptionIfWarriorAlreadyEnrolled()
+        {
+            //Arrange and Act
+            var warrior = new Warrior("Artur", 5, 35);
+            this.arena.Enroll(warrior);
+            //Assert
+            Assert.AreEqual(warrior,this.arena.Warriors.First());
+            Assert.AreEqual(1,this.arena.Count);
+            Assert.Throws<InvalidOperationException>(() =>
+                this.arena.Enroll(warrior),
+                "Warrior is already enrolled for the fights!");
+        }
+
+        [TestCase("Hitman", "BussyToCome")]
+        [TestCase("BussyToCome", "Defender")]
+        public void FightingShouldThrowExceptionIfOneOfWarriorsIsNotEnrolled(string attackerName,string defenderName)
+        {
+            //Arrange
+            var attacker = new Warrior("Hitman", 1, 100);
+            var defender = new Warrior("Defender", 1, 100);
+            //Act
+            arena.Enroll(attacker);
+            arena.Enroll(defender);
+            //Assert
+            Assert.Throws<InvalidOperationException>(() => 
+                arena.Fight(attackerName,defenderName), "There is no fighter with name BussyToCome enrolled for the fights!");
+        }
+
+        [Test]
+        public void FightShouldCallAttackMethodIfTwoNamesAreEnrolled()
+        {
+            //Arrange
+            var attacker = new Warrior("Hitman", 1, 100);
+            var defender = new Warrior("Defender", 1, 100);
+            //Act
+            arena.Enroll(attacker);
+            arena.Enroll(defender);
+            arena.Fight("Hitman","Defender");
+            Assert.AreEqual(99,attacker.HP);
+            Assert.AreEqual(99,defender.HP);
+        }
 
     }
 }
--- UnitTesting-Exercises/FightingArena.Tests/WarriorTests.cs
+++ UnitTesting-Exercises/FightingArena.Tests/WarriorTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Reflection.Metadata;
+
 namespace FightingArena.Tests
 {
     using NUnit.Framework;
@@ -5,6 +8,97 @@ namespace FightingArena.Tests
     [TestFixture]
     public class WarriorTests
     {
+        private Warrior warrior;
 
+        [SetUp]
+        public void Start()
+        {
+            this.warrior = new Warrior("Mich", 10, 50);
+        }
+
+        //Testing ctor 
+        [Test]
+        public void ConstructorShouldWorkProperly()
+        {
+            Assert.AreEqual("Mich", this.warrior.Name);
+            Assert.AreEqual(10, this.warrior.Damage);
+            Assert.AreEqual(50, this.warrior.HP);
+        }
+
+        //Testing Property setters` Validations
+        [TestCase(" ")]
+        [TestCase(null)]
+        [TestCase("")]
+        public void PropertyNameValidationShouldThrowExceptionWhenNullOrWhiteSpace(string name)
+        {
+            Assert.Throws<ArgumentException>(() =>
+                    new Warrior(name, 10, 50),
+                "Name should not be empty or whitespace!");
+        }
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void PropertyDamageValidationShouldThrowExceptionWhenNegativeOr0(int dmg)
+        {
+            Assert.Throws<ArgumentException>(() =>
+                    new Warrior("Mich", dmg, 50),
+                "Damage value should be positive!");
+        }
+
+        [Test]
+        public void PropertyHPValidationShouldThrowExceptionWhenNegative()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                new Warrior("Mich", 10, -1),
+                "HP should not be negative!");
+        }
+
+        //Testing Attack method
+        [TestCase(30)]
+        [TestCase(29)]
+        public void WarriorShouldNotAttackWhenHPLessThanMinThresh(int hp)
+        {
+            var warrior = new Warrior("Weaky", 1, hp);
+            Assert.Throws<InvalidOperationException>(() =>
+                warrior.Attack(warrior),
+                "Your HP is too low in order to attack other warriors!");
+        }
+        [TestCase(30)]
+        [TestCase(29)]
+        public void WarriorShouldNotAttackWhenHPDefWarriorLessThanMinThresh(int hp)
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+                     this.warrior.Attack(new Warrior("Weaky", 1, hp)),
+                 "Enemy HP must be greater than 30 in order to attack him!");
+        }
+
+        [Test]
+        public void WarriorShouldNotAttackWhenHPLessThanDefDamage()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+                    this.warrior.Attack(new Warrior("GodsDamage", 51, 31)),
+                "You are trying to attack too strong enemy");
+        }
+
+
+        [TestCase(10, 33, 40)]
+        [TestCase(50, 33, 0)]
+        public void WarriorShouldTakeDmgFromDefender(int dmg, int hp, int expectedHp)
+        {
+            var defender = new Warrior("Hitman", dmg, hp);
+            warrior.Attack(defender);
+            Assert.AreEqual(expectedHp, this.warrior.HP);
+
+        }
+
+        [TestCase(50, 0)]
+        [TestCase(30, 20)]
+        [TestCase(51, 0)]
+        public void WarriorShouldDecreaseDefHpProperly(int dmg, int defHP)
+        {
+            var attacker = new Warrior("Hitman", dmg, 100);
+            attacker.Attack(this.warrior);
+            Assert.AreEqual(defHP, this.warrior.HP);
+        }
     }
 }
